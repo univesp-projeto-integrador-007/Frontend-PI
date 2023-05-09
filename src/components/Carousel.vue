2b4ec56e60b28bfce9767cb9b6bed1318271b14f
gdantasc <gabrieldcavalcante00@gmail.com>
2023-05-08 21:39:41 -0300

    feat: carousel

---
+++ src/components/Carousel.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="carousel">
+    <div class="parent">
+      <div class="one">
+        <div class="one__container">
+          <h1>A doceria onde o principal ingrediente é amor.</h1>
+          <p>Feito com frutas selecionadas</p>
+          <Button text="Ver cardápio" />
+        </div>
+      </div>
+      <div class="two"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Button from "@/components/Button.vue";
+
+export default {
+  name: "Carousel",
+  components: { Button },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/scss/main.scss";
+
+.carousel {
+  width: 100%;
+  height: 400px;
+}
+
+.parent {
+  position: relative;
+  width: 100%;
+}
+.one {
+  position: absolute;
+  height: 400px;
+  width: 75%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  @media (max-width: $desktop) {
+    width: 55%;
+  }
+
+  &__container {
+    width: 470px;
+    text-align: left;
+
+    h1 {
+      line-height: 40px;
+      font-size: 36px;
+      font-weight: 700;
+      color: $yellow-50;
+      margin-bottom: 8px;
+    }
+
+    p {
+      font-size: 22px;
+      font-weight: 400;
+      color: $orange-50;
+      margin-bottom: 40px;
+    }
+  }
+}
+.two {
+  position: absolute;
+  height: 400px;
+  width: 65%;
+  @media (min-width: $desktop) {
+    width: 43%;
+  }
+}
+.one {
+  left: 0;
+  top: 0;
+  background: $secondary;
+}
+.two {
+  right: 0;
+  top: 0;
+  background-image: url("@/assets/candy.svg");
+  -webkit-clip-path: polygon(30% 0, 100% 0%, 100% 100%, 0% 100%);
+  clip-path: polygon(30% 0, 100% 0%, 100% 100%, 0% 100%);
+}
+</style>
