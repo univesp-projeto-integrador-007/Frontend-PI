5379fcfbf847ebc7be48e7bc2bab653a8c203cd0
gdantasc <gabrieldcavalcante00@gmail.com>
2023-05-18 19:53:09 -0300

    feat: about view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,36 +1,29 @@
 <template>
   <div class="about">
-    <h1>Quem somos</h1>
-    <p>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-      Lorem Ipsum has been the industry's standard dummy text ever since the
-      1500s, when an unknown printer took a galley of type and scrambled it to
-      make a type specimen book. It has survived not only five centuries, but
-      also the leap into electronic typesetting, remaining essentially
-      unchanged. It was popularised in the 1960s with the release of Letraset
-      sheets containing Lorem Ipsum passages, and more recently with desktop
-      publishing software like Aldus PageMaker including versions of Lorem
-      Ipsum. Why do we use it? It is a long established fact that a reader will
-      be distracted by the readable content of a page when looking at its
-      layout. The point of using Lorem Ipsum is that it has a more-or-less
-      normal distribution of letters, as opposed to using 'Content here, content
-      here', making it look like readable English. Many desktop publishing
-      packages and web page editors now use Lorem Ipsum as their default model
-      text, and a search for 'lorem ipsum' will uncover many web sites still in
-      their infancy. Various versions have evolved over the years, sometimes by
-      accident, sometimes on purpose (injected humour and the like). Where does
-      it come from? Contrary to popular belief, Lorem Ipsum is not simply random
-      text. It has roots in a piece of classical Latin literature from 45 BC,
-      making it over 2000 years old. Richard McClintock, a Latin professor at
-      Hampden-Sydney College in Virginia, looked up one of the more obscure
-      Latin words, consectetur, from a Lorem Ipsum passage, and going through
-      the cites of the word in classical literature, discovered the undoubtable
-      source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus
-      Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in
-      45 BC. This book is a treatise on the theory of ethics, very popular
-      during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor
-      sit amet..", comes from a line in section 1.10.32.
-    </p>
+    <div class="about__container">
+      <h1>Sobre nós</h1>
+      <div class="about__wrapper" />
+      <p>
+        A D.Brigadeiros é uma empresa que nasceu da paixão pela culinária e pelo
+        sabor irresistível dos brigadeiros, bolos e doces. Fundada por uma
+        empreendedora que sempre buscou oferecer produtos de qualidade, a
+        empresa tem o objetivo de levar aos seus clientes doces deliciosos e
+        únicos, feitos com muito carinho e dedicação.
+      </p>
+      <p>
+        Nossos produtos são preparados com ingredientes selecionados e de alta
+        qualidade, resultando em um sabor inigualável e textura perfeita. Além
+        disso, nossa produção é artesanal, com muito cuidado e atenção aos
+        detalhes, garantindo a excelência em cada uma de nossas criações.
+      </p>
+      <p>
+        Por tudo isso, a D.Brigadeiros se tornou referência em qualidade e
+        sabor, conquistando cada vez mais clientes e admiradores. Nosso
+        compromisso é continuar oferecendo produtos irresistíveis e um
+        atendimento excepcional, para que nossos clientes possam desfrutar do
+        melhor que a vida tem a oferecer.
+      </p>
+    </div>
   </div>
 </template>
 
@@ -38,8 +31,45 @@
 <style lang="scss" scoped>
 @import "@/scss/main.scss";
 .about {
-  h1 {
-    font-size: 2rem;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 180px;
+
+  &__wrapper {
+    width: 50%;
+    background: $yellow-150;
+    height: 2px;
+    margin: 16px 0 95px;
+  }
+
+  &__container {
+    margin-top: 22px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 80%;
+    width: 80%;
+    text-align: center;
+    background: $yellow-100;
+    padding: 20px 43px 100px;
+    max-width: 1060px;
+
+    h1 {
+      font-size: 2rem;
+      font-weight: 600;
+      color: $primary;
+    }
+
+    p {
+      font-size: 1.3rem;
+      font-weight: 400;
+      color: $secondary;
+      margin-bottom: 20px;
+    }
   }
 }
 </style> 
