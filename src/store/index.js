8f997f62e5bf053aa4ddbc5335e3f8be8eca0881
gdantasc <gabrieldcavalcante00@gmail.com>
2023-05-18 19:54:56 -0300

    feat: store Vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,26 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-  },
-  getters: {
+    currentProduct: {},
+    cart: [],
   },
   mutations: {
+    storeCurrentProduct(state, data) {
+      state.currentProduct = data;
+    },
+    storeCart(state, data) {
+      state.cart.push(data);
+    },
+    storeRemoveCart(state, data) {
+      const indice = state.cart.indexOf(data);
+      state.cart.splice(indice, 1);
+    },
+    storeCleanCart(state){
+      state.cart = []
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
 })
