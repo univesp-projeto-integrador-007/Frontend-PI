e1fa05bf24331f9c7f6591145cd1f1db8eebeaf6
gdantasc <gabrieldcavalcante00@gmail.com>
2023-05-18 19:54:19 -0300

    feat: pedidos view

---
+++ src/views/PedidosView.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="orders">
+    <h1>Pedidos:</h1>
+
+    <div v-for="order in orders" :key="order._id">
+      <OrderCard :order="order" />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import OrderCard from "@/components/OrderCard.vue";
+
+export default {
+  name: "PedidosView",
+  components: { OrderCard },
+  data() {
+    return {
+      orders: [],
+    };
+  },
+
+  async mounted() {
+    try {
+      const tokenArmazenado = localStorage.getItem("token");
+      if (!tokenArmazenado) {
+        return this.$router.push("/");
+      }
+      const res = await axios.get("http://localhost:3000/api/orders", {
+        headers: {
+          Authorization: `Bearer ${tokenArmazenado}`,
+        },
+      });
+      this.orders = res.data;
+    } catch (err) {
+      console.log(err.response.data.msg);
+    }
+  },
+
+  methods: {
+    HandleProduct(value) {
+      this.$store.commit("storeCurrentProduct", value);
+      this.$router.push("/produtos");
+    },
+    handleFilter(value) {
+      this.filter = value;
+    },
+    itensUnicos(arr) {
+      return arr.filter((v, i, a) => a.indexOf(v) === i);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/scss/main.scss";
+
+.orders {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+
+  height: 100%;
+  padding-bottom: 180px;
+
+  h1 {
+    font-size: 32px;
+    font-weight: 600;
+    color: $primary;
+    margin-bottom: 30px;
+  }
+}
+</style>
