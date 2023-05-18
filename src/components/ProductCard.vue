fb71d2eb13df0ebc45b5388f4e29dc993eb120ac
gdantasc <gabrieldcavalcante00@gmail.com>
2023-05-18 19:52:40 -0300

    feat: product card

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,19 +1,30 @@
 <template>
   <div class="product-card">
-    <img class="product-card__image" src="@/assets/cake-example.jpg" alt="" />
+    <img class="product-card__image" :src="product.image" alt="" />
     <div class="product-card__content">
-      <span class="product-card__content__type">DOCE DE POTE</span>
-      <h1 class="product-card__content__title">Delícia de KitKat</h1>
-      <p class="product-card__content__text">R$ 7,00</p>
+      <span class="product-card__content__type">{{ product.type }}</span>
+      <h1 class="product-card__content__title">{{ product.name }}</h1>
+      <p class="product-card__content__text">
+        {{ maskedMoney(product.price) }}
+      </p>
     </div>
   </div>
 </template>
 
 <script>
 import Button from "@/components/Button.vue";
+import ConvertMoney from "@/helpers/convert-money";
 export default {
   name: "ProductCard",
   components: { Button },
+  props: {
+    product: { type: Object, required: true },
+  },
+  methods: {
+    maskedMoney(value) {
+      return ConvertMoney(value);
+    },
+  },
 };
 </script>
 
@@ -28,9 +39,10 @@ export default {
   background: transparent;
   border-radius: 10px;
   border: 2px solid $yellow-100;
+  cursor: pointer;
 
   &__image {
-    width: 195px;
+    height: 120px;
     border-radius: 10px 10px 5px 5px;
     -webkit-mask-image: linear-gradient(to top, transparent 0%, black 10%);
   }
@@ -44,14 +56,14 @@ export default {
 
     &__type {
       font-weight: 700;
-      font-size: 8px;
+      font-size: 12px;
       color: $brown-100;
       margin-bottom: 4px;
     }
 
     &__title {
       font-weight: 400;
-      font-size: 14px;
+      font-size: 16px;
       color: $primary;
       margin-bottom: 7px;
     }
